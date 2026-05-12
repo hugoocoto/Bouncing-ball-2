519b8e19ca97fce4b01109afaadd31756e27ee60
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-05-12 08:48:37 +0200

    collisions

--- main.c
+++ main.c
@@ -12,12 +12,17 @@ typedef struct {
         Vector2 velocity;
         float radius;
         Color color;
+        struct {
+                bool hold;
+                Vector2 prev;
+                double prev_t;
+                Vector2 acel;
+        } misc;
 } Obj;
 
 typedef struct {
         Vector2 pos;
 } Cam;
-
 
 // world to view
 Vector2
@@ -50,6 +55,81 @@ obj_draw(Cam *cam, Obj *o)
         DrawCircleV(WTV(cam, o->world), o->radius, o->color);
 }
 
+void
+check_collisions(Obj *o1, Obj *o2)
+{
+        if (CheckCollisionCircles(o1->world, o1->radius, o2->world, o2->radius)) {
+                Vector2 tmp = o1->velocity;
+                o1->velocity = o2->velocity;
+                o2->velocity = tmp;
+        }
+}
+
+void
+recalc_obj_positions(Cam *cam, Obj *o)
+{
+        static bool some_hold = false;
+        // hold
+        if (!some_hold && IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+                o->misc.hold = o->misc.hold || CheckCollisionPointCircle(GetMousePosition(), WTV(cam, o->world), o->radius);
+                some_hold = o->misc.hold;
+                o->misc.prev = GetMousePosition();
+                o->misc.prev_t = GetTime();
+                o->misc.acel = (Vector2) { 0, 0 };
+        }
+
+        // throw (release ball)
+        if (o->misc.hold && IsMouseButtonUp(MOUSE_LEFT_BUTTON)) {
+                o->misc.hold = false;
+                some_hold = false;
+                o->velocity.x = o->misc.acel.x;
+                o->velocity.y = o->misc.acel.y;
+        }
+
+        // follow mouse and calc acel vector for throw
+        if (o->misc.hold) {
+                Vector2 mouse_pos = GetMousePosition();
+                o->world = VTW(cam, mouse_pos);
+                double dpt = 0.006 / (GetTime() - o->misc.prev_t);
+                o->misc.acel.x += (GetMousePosition().x - o->misc.prev.x) * dpt;
+                o->misc.acel.y += (GetMousePosition().y - o->misc.prev.y) * dpt;
+                o->misc.acel.x += -o->misc.acel.x * dpt;
+                o->misc.acel.y += -o->misc.acel.y * dpt;
+                o->misc.prev = mouse_pos;
+                o->misc.prev_t = GetTime();
+        }
+
+        // free bouncing
+        else {
+                if (o->velocity.y < 1) o->velocity.y += 0.3 * GetFrameTime() * 70;
+                if (o->velocity.x != 0) o->velocity.x -= o->velocity.x * 0.002 * GetFrameTime() * 70;
+                o->velocity.y += 1 * GetFrameTime() * 70;
+                o->world.x += o->velocity.x * GetFrameTime() * 70;
+                o->world.y += o->velocity.y * GetFrameTime() * 70;
+
+                Vector2 off = GetWindowPosition();
+                Vector2 wsz = (Vector2) { GetScreenWidth(), GetScreenHeight() };
+
+                if (o->world.x < off.x + o->radius) {
+                        o->velocity.x = fabs(o->velocity.x);
+                        o->world.x = off.x + o->radius;
+                }
+                if (o->radius + o->world.x - off.x > wsz.x) {
+                        o->velocity.x = -fabs(o->velocity.x);
+                        o->world.x = wsz.x + off.x - o->radius;
+                }
+                if (o->world.y < o->radius + off.y) {
+                        o->velocity.y = fabs(o->velocity.y);
+                        o->world.y = o->radius + off.y;
+                }
+                if (o->radius + o->world.y - off.y > wsz.y) {
+                        o->velocity.y = -fabs(o->velocity.y);
+                        o->world.y = wsz.y + off.y - o->radius;
+                }
+        }
+}
+
+
 int
 main(int argc, char **argv)
 {
@@ -63,77 +143,27 @@ main(int argc, char **argv)
                 .world = (Vector2) { WIDTH / 2., HEIGHT / 2. },
                 .velocity = (Vector2) { 10, 10 },
         };
-        Cam c = { 0 };
 
-        bool hold = false;
-        Vector2 prev;
-        double prev_t;
-        Vector2 acel;
+        Obj o2 = {
+                .color = BLUE,
+                .radius = 40,
+                .world = (Vector2) { WIDTH / 2., HEIGHT / 2. },
+                .velocity = (Vector2) { 20, 10 },
+        };
+
+        Cam c = { 0 };
 
 
         while (!WindowShouldClose()) {
-                // hold
-                if (!hold && IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                        hold = hold || CheckCollisionPointCircle(GetMousePosition(), WTV(&c, o.world), o.radius);
-                        prev = GetMousePosition();
-                        prev_t = GetTime();
-                        acel = (Vector2) { 0, 0 };
-                }
-
-                // throw (release ball)
-                if (hold && IsMouseButtonUp(MOUSE_LEFT_BUTTON)) {
-                        hold = false;
-                        o.velocity.x = acel.x;
-                        o.velocity.y = acel.y;
-                }
-
-                // follow mouse and calc acel vector for throw
-                if (hold) {
-                        Vector2 mouse_pos = GetMousePosition();
-                        o.world = VTW(&c, mouse_pos);
-                        double dpt = 0.006 / (GetTime() - prev_t);
-                        acel.x += (GetMousePosition().x - prev.x) * dpt;
-                        acel.y += (GetMousePosition().y - prev.y) * dpt;
-                        acel.x += -acel.x * dpt;
-                        acel.y += -acel.y * dpt;
-                        prev = mouse_pos;
-                        prev_t = GetTime();
-                }
-
-                // free bouncing
-                else {
-                        if (o.velocity.y < 1) o.velocity.y += 0.3 * GetFrameTime() * 70;
-                        if (o.velocity.x != 0) o.velocity.x -= o.velocity.x * 0.002 * GetFrameTime() * 70;
-                        o.velocity.y += 1 * GetFrameTime() * 70;
-                        o.world.x += o.velocity.x * GetFrameTime() * 70;
-                        o.world.y += o.velocity.y * GetFrameTime() * 70;
-
-                        Vector2 off = GetWindowPosition();
-                        Vector2 wsz = (Vector2) { GetScreenWidth(), GetScreenHeight() };
-
-                        if (o.world.x < off.x + o.radius) {
-                                o.velocity.x = fabs(o.velocity.x);
-                                o.world.x = off.x + o.radius;
-                        }
-                        if (o.radius + o.world.x - off.x > wsz.x) {
-                                o.velocity.x = -fabs(o.velocity.x);
-                                o.world.x = wsz.x + off.x - o.radius;
-                        }
-                        if (o.world.y < o.radius + off.y) {
-                                o.velocity.y = fabs(o.velocity.y);
-                                o.world.y = o.radius + off.y;
-                        }
-                        if (o.radius + o.world.y - off.y > wsz.y) {
-                                o.velocity.y = -fabs(o.velocity.y);
-                                o.world.y = wsz.y + off.y - o.radius;
-                        }
-                }
-
+                recalc_obj_positions(&c, &o);
+                recalc_obj_positions(&c, &o2);
+                check_collisions(&o, &o2);
 
                 BeginDrawing();
                 ClearBackground(BLACK);
 
                 obj_draw(&c, &o);
+                obj_draw(&c, &o2);
                 EndDrawing();
         }
 
